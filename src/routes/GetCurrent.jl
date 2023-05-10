fd1046fd513dd9cde27a28db740184a283759e9d
sergeypypyrev <sergey@pypyrev.com>
2023-05-10 20:07:49 +0700

    Update GetCurrent.jl

--- src/routes/GetCurrent.jl
+++ src/routes/GetCurrent.jl
@@ -5,6 +5,7 @@ using Genie.Renderer, ..FileUpload, ..TextUpdate
 function handler()
 	result = FileUpload.getCurrent()
 	merge!(result, TextUpdate.getCurrent())
+	Genie.WebChannels.unsubscribe_disconnected_clients()
 	return Renderer.Json.JSONParser.json(result)
 end
 
